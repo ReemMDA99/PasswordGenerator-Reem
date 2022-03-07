2761223fd0360c2a19d735c2fd295a864828f1ed
Reem Ahmed <reem.mda0909@gmail.com>
2022-03-07 04:36:44 -0500

    password generated

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,63 @@
-/*// Assignment Code
-var generateBtn = document.querySelector("#generate");
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboard = document.getElementById('clipboard');
 
-  passwordText.value = password;
-
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-*/
 
-const keys = {
-  includeUppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  includeLowercase: "abcdefghijklmnopqrstuvwxyz",
-  includeNumbers: "0123456789",
-  includeSymbols: " !#$%&'()*+,-./:;<=>?@[^_`{|}~"
-}
-const getKey = [
-  function includeUppercase() {
-    return keys.includeUppercase[Math.floor(Math.random() * keys.includeUppercase.length)];
-  },
-  function includeLowercase() {
-    return keys.includeLowercase[Math.floor(Math.random() * keys.includeLowercase.length)];
-  },
-  function includeNumbers() {
-    return keys.includeNumbers[Math.floor(Math.random() * keys.includeNumbers.length)];
-  },
-  function includeSymbols() {
-    return keys.includeSymbols[Math.floor(Math.random() * keys.includeSymbols.length)];
-  }
-];
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value;
+    const hasLower = lowercaseEl.checked;
+    const hasUpper = uppercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
 
-function createPassword() {
-  const includeUppercase = document.getElementById("includeUppercase").checked;
-  const includeLowercase = document.getElementById("includeLowercase").checked;
-  const includeNumbers = document.getElementById("includeNumbers").checked;
-  const includeSymbols = document.getElementById("includeSymbols").checked;
-  if (includeUppercase + includeLowercase + includeNumbers + includeSymbols === 0) {
-    alert("Please check atleast one box!");
-    return;
-  }
-  const passwordBox = document.getElementById("passwordBox");
-  const length = document.getElementById("length");
-  let password = "";
-  while (length.value > password.length) {
-    let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)];
-    let isChecked = document.getElementById(keyToAdd.name).checked;
-    if (isChecked) {
-      password += keyToAdd();
+    resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typeArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+    if(typesCount === 0) {
+        return '';
     }
-  }
-  
- return passwordBox.innerHTML = password();
-};
+
+    for(let i=0; i<length; i+=typesCount) {
+        typeArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+
+    return finalPassword;
+}
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*()<>[]{}/,.='
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
